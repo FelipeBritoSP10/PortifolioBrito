935f004a91d92f532a611071d210ce3b80fefbba
Felipe Brito <139879477+FelipeBritoSP10@users.noreply.github.com>
2024-03-23 12:55:28 -0300

    Update moviment.js

--- moviment.js
+++ moviment.js
@@ -1,12 +1,13 @@
-                    //Menu hamburguer
+                    //Menu de Hamburguer
 document.addEventListener("DOMContentLoaded", function() {
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
 
-menuToggle.addEventListener('click', function() {
-menu.classList.toggle('hidden');
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('hidden');
     });
 });
+
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         loop:true,
@@ -20,4 +21,14 @@ $(document).ready(function(){
             }
         }
     });
+});
+
+//Biblioteca ScrollReveal
+ScrollReveal().reveal('*', {
+    delay: 200,    
+    duration: 1000, 
+    origin: 'bottom', 
+    distance: '20px',
+    easing: 'ease-in-out',
+    interval: 100 
 });
