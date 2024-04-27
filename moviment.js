8459930bee51ff917c095e45b7d344f2cdd3a25e
Felipe Brito <139879477+FelipeBritoSP10@users.noreply.github.com>
2024-04-27 15:53:01 -0300

    Update moviment.js

--- moviment.js
+++ moviment.js
@@ -46,3 +46,22 @@ $(document).ready(function(){
       },
     });
   });
+  $(document).ready(function() {
+            $(".owl-carousel").owlCarousel({
+                loop: true,
+                margin: 20,
+                nav: false,
+                dots: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 3
+                    }
+                }
+            });
+        });
