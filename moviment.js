4304b412f4d30fda930aa35f5bc873bc3b2e045a
Felipe Brito <139879477+FelipeBritoSP10@users.noreply.github.com>
2024-04-27 14:20:29 -0300

    Update moviment.js

--- moviment.js
+++ moviment.js
@@ -35,9 +35,14 @@ $(document).ready(function(){
             });
         });
 
- var swiper = new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   });
